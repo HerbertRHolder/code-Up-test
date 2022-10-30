18caf029505eac6d9e0a57507a2deafc52c32de0
Herb <herbrholder@gmail.com>
2022-10-30 13:35:24 -0500

    Refactored my functions to be more efficient

--- solutions.js
+++ solutions.js
@@ -1,30 +1,19 @@
 // use strict
 function isNum(x){
-   return isNaN(parseFloat(x))
+   return !isNaN(parseFloat(x))
 }
 
 function isBool (x){
     return (typeof x === "boolean");
 }
-function isNull(x){
-    return x === null;
-}
+
 function isString(x){
     return typeof x === 'string';
 }
-function isUndefined(x){
-    return x === undefined;
-}
+
 function isNeg(x){
     return isNum(x) && (x < 0);
 }
-function isArray(x){
-    return (x instanceof Array);
-}
-function isObject(x){
-    return (x instanceof Object);
-}
-
 function lowerCase(a){
    if(isString(a)){
     return a.toLowerCase();
@@ -33,7 +22,10 @@ function lowerCase(a){
    return false;
 }
 function isAllLowerCase(a){
-    
+    if (isString(a)){
+        return a.toLowerCase()=== a;
+    }
+    return false;
 }
 function isAllUpperCase(a){
     if (!isUndefined(a) && !isNull(a) && !isNull(a) && !isBool(a) && !isObject(a) && !isArray(a) && isString(a)){
@@ -41,63 +33,60 @@ function isAllUpperCase(a){
     }else {return false;}
 }
 function isNotPalindrome(a) {
-    if (a[0] !== a.toUpperCase() && !isUndefined(a) && !isNull(a) && !isBool(a) && !isObject(a) && !isArray(a) && isString(a) && !isNum(a))
-    {
-        let arr = a.split('');
-         arr = arr.reverse();
-       return !(arr.join('') === a);
+    let arr = [];
+   if (isString(a)){
+    arr = a.split('');
+    arr.reverse();
+    arr = arr.join('');
+    return (arr.toUpperCase() !== a.toUpperCase())
 
-    }else {return true;}
+    
+   }
 
+   return true;
 }
 
 function multiplyBy2(a){
-    if (!isUndefined(a) && !isNull(a) && !isBool(a) && !isObject(a)
-        && !isArray(a) && !isNaN(a)){
-        return parseInt(`${a*2}`);
-    }else {return false;}
+   if (isNum(a)){
+    return a*2;
+   }
+   return false;
 }
 
 function convertHourToSec(a){
-    if (!isUndefined(a) && !isNull(a) && !isBool(a) && !isObject(a)
-        && !isArray(a) && !isNaN(a) && !isNeg(a)){
-        return (a*60)*60;
-    }else {return false;}
+    if (isNum(a) && !isNeg(a)){
+        return a*60*60;
+       }
+       return false;
 }
 
 function getLowestNumber(a, b, c){
-    if (isArray(a) || isArray(b) || isArray(c))
-    {
-        return false;
-    }else if (!isUndefined(a) && !isNull(a) && !isBool(a) && !isObject(a)
-        && !isArray(a) && !isNaN(a) && isNum(a) === isNum(b) === isNum(c) || isNeg(a) || isNeg(b) || isNeg(c)){
-        return Math.min(a, b, c);
-    }else {return false;}
+   if (isNum(a) && isNum(b) && isNum(c)){
+    return Math.min(a, b, c);
+   }
+   return false;
 }
 function addStringLengths(a, b){
-    if (isUndefined(a) || isUndefined(b) || isNum(a) || isNum(b) || isNull(a) || isNull(b) || isBool(b)
-        || isBool(a) || isObject(a) || isObject(b) || isArray(a) || isArray(b)){
-        return false;
-    }else {
+    if (isString(a) && isString(b)){
         return a.length + b.length;
     }
+    return false;
 }
 
 function subtract(a, b){
-    if (isUndefined(a) || isUndefined(b) || isNaN(a) || isNaN(b) || isNull(a) || isNull(b) || isBool(b)
-        || isBool(a) || isObject(a) || isObject(b) || isArray(a) || isArray(b)){
-        return false;
-    }else{return a-b;}
-
+   if (isNum(a) && isNum(b)){
+        return a-b;
+   }
+return false;
 }
 function calculateChange(t, c){
-    if (isUndefined(t) || isUndefined(c) || !isNaN(t) || !isNaN(c) || isNull(t) || isNull(c) || isBool(t)
-        || isBool(c) || isObject(t) || isObject(c) || isArray(t) || isArray(c)){
-        return false;
-    }else{return "$" + parseFloat(`${t-c}`);}
-
-
+   if (isNum(t) && isNum(c)){
+    t = parseFloat(t);
+    c = parseFloat(c);
+        return `$${parseFloat(t-c)}`;
+   }
+   return false;
 }
 
-// 
+
 
